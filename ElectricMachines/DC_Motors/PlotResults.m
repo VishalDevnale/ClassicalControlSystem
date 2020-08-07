87f9215b83497434a0e6555997806dc4636f3ea4
Vishal Devnale <56331836+VishalDevnale@users.noreply.github.com>
2020-08-07 13:32:13 -0400

    Plot Function Modified to show steady state and peak values.

--- ElectricMachines/DC_Motors/PlotResults.m
+++ ElectricMachines/DC_Motors/PlotResults.m
@@ -48,3 +48,45 @@ figure;
         xlabel('time (s)');
 
 findfigs
+
+%% Steady state values
+
+ss_i_f = out.i_f.data(size(out.i_f.data,1));
+ss_i_a = out.i_a.data(size(out.i_a.data,1));
+ss_TE = out.TE.data(size(out.TE.data,1));
+ss_TL = out.TL.data(size(out.TL.data,1));
+ss_omega = out.omega.data(size(out.omega.data,1));
+
+max_i_f = max(out.i_f.data);
+max_i_a = max(out.i_a.data);
+max_TE = max(out.TE.data);
+max_TL = max(out.TL.data);
+max_omega = max(out.omega.data);
+
+Comment_subtitle = 'Steady state values:';
+Comment_ss_i_f = ['Field current (i_f): ',num2str(ss_i_f), ' amp'];
+Comment_ss_i_a = ['Armature current (i_a): ',num2str(ss_i_a), ' amp'];
+Comment_ss_TE = ['Electric Torque (TE): ',num2str(ss_TE), ' Nm'];
+Comment_ss_TL = ['Load Torque (TL): ',num2str(ss_TL), ' Nm'];
+Comment_ss_omega = ['Angular Velcodity (omega): ',num2str(ss_omega), ' rad/sec'];
+
+% Msg box of comments
+MSGBOX = msgbox({Comment_subtitle,Comment_ss_i_f, Comment_ss_i_a, Comment_ss_TE, Comment_ss_TL, Comment_ss_omega},'Comments');
+set(MSGBOX, 'position', [100 220 550 150]);
+ah = get( MSGBOX, 'CurrentAxes');
+ch = get( ah, 'Children');
+set( ch, 'FontSize', 15); 
+
+Comment_subtitle = 'Maximum/Peak values:';
+Comment_max_i_f = ['Field current (i_f): ',num2str(max_i_f), ' amp'];
+Comment_max_i_a = ['Armature current (i_a): ',num2str(max_i_a), ' amp'];
+Comment_max_TE = ['Electric Torque (TE): ',num2str(max_TE), ' Nm'];
+Comment_max_TL = ['Load Torque (TL): ',num2str(max_TL), ' Nm'];
+Comment_max_omega = ['Angular Velcodity (omega): ',num2str(max_omega), ' rad/sec'];
+
+% Msg box of comments
+MSGBOX = msgbox({Comment_subtitle,Comment_max_i_f, Comment_max_i_a, Comment_max_TE, Comment_max_TL, Comment_max_omega},'Comments');
+set(MSGBOX, 'position', [100 220 550 150]);
+ah = get( MSGBOX, 'CurrentAxes');
+ch = get( ah, 'Children');
+set( ch, 'FontSize', 15); 
