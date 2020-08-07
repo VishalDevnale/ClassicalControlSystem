41bdc5d4edf7d76dadc900245f37fcf98e095337
Vishal Devnale <56331836+VishalDevnale@users.noreply.github.com>
2020-08-07 17:25:55 -0400

    Folder structure corrected according to problems.

--- ElectricMachines/DC_Motors/01_SeperatelyExcitedDCMotor/01_AbruptStart/PlotResults.m
+++ ElectricMachines/DC_Motors/01_SeperatelyExcitedDCMotor/01_AbruptStart/PlotResults.m
@@ -1,7 +1,10 @@
-numplot = 5;            % Number of Subplots
+close all;
+
+numplot = 3;            % Number of Subplots
 factor = 1000;          % to convert to milli
 
 figure;
+%set(figure, 'position', [0 1000 2048/3 600]);
     subplot(numplot,1,1)
         plot(out.i_f,'LineWidth',2)
         grid on
@@ -28,8 +31,12 @@ figure;
         legend('w (rad/s)','Location','SouthEast')
         ylabel('Omega (rad/sec)');
         xlabel('time (s)');
-        
-    subplot(numplot,1,4)
+findfigs
+
+numplot = 2;            % Number of Subplots
+
+figure;
+    subplot(numplot,1,1)
         plot(out.TE,'LineWidth',2)
         grid on
         plottitle = sprintf('%s%s', 'Plot Electric Torque ','of System ');
@@ -38,7 +45,7 @@ figure;
         ylabel('Electric Torque (Nm)');
         xlabel('time (s)');
     
-    subplot(numplot,1,5)
+    subplot(numplot,1,2)
         plot(out.TL,'LineWidth',2)
         grid on
         plottitle = sprintf('%s%s', 'Plot Load Torque ','of System ');
@@ -47,6 +54,68 @@ figure;
         ylabel('Load Torque (Nm)');
         xlabel('time (s)');
 
+findfigs
+
+numplot = 3;            % Number of Subplots
+
+figure;
+    subplot(numplot,1,1)
+        plot(out.ElectricEnergy,'LineWidth',2)
+        grid on
+        plottitle = sprintf('%s%s', 'Plot Electric Energy ','of System ');
+        title(plottitle)
+        legend('Electric Energy (kJ)','Location','SouthEast')
+        ylabel('Electric Energy (kJ)');
+        xlabel('time (s)');
+
+    subplot(numplot,1,2)
+        plot(out.MechanicalEnergy,'LineWidth',2)
+        grid on
+        plottitle = sprintf('%s%s', 'Plot Mechanical Energy ','of System ');
+        title(plottitle)
+        legend('Mechanical Energy (kJ)','Location','SouthEast')
+        ylabel('Mechanical Energy (kJ)');
+        xlabel('time (s)');
+
+    subplot(numplot,1,3)
+        plot(out.LostEnergy,'LineWidth',2)
+        grid on
+        plottitle = sprintf('%s%s', 'Plot Lost Energy ','of System ');
+        title(plottitle)
+        legend('Lost Energy (kJ)','Location','SouthEast')
+        ylabel('Lost Energy (kJ)');
+        xlabel('time (s)');
+findfigs
+
+numplot = 3;            % Number of Subplots
+
+figure;
+    subplot(numplot,1,1)
+        plot(out.ElectricPower,'LineWidth',2)
+        grid on
+        plottitle = sprintf('%s%s', 'Plot Electric Power ','of System ');
+        title(plottitle)
+        legend('Electric Power (kW)','Location','SouthEast')
+        ylabel('Electric Power (kW)');
+        xlabel('time (s)');
+
+    subplot(numplot,1,2)
+        plot(out.MechanicalPower,'LineWidth',2)
+        grid on
+        plottitle = sprintf('%s%s', 'Plot Mechanical Power ','of System ');
+        title(plottitle)
+        legend('Mechanical Power (kW)','Location','SouthEast')
+        ylabel('Mechanical Power (kW)');
+        xlabel('time (s)');
+
+    subplot(numplot,1,3)
+        plot(out.LostPower,'LineWidth',2)
+        grid on
+        plottitle = sprintf('%s%s', 'Plot Lost Power ','of System ');
+        title(plottitle)
+        legend('Lost Power (kW)','Location','SouthEast')
+        ylabel('Lost Power (kW)');
+        xlabel('time (s)');
 findfigs
 
 %% Steady state values
@@ -72,7 +141,7 @@ Comment_ss_omega = ['Angular Velcodity (omega): ',num2str(ss_omega), ' rad/sec']
 
 % Msg box of comments
 MSGBOX = msgbox({Comment_subtitle,Comment_ss_i_f, Comment_ss_i_a, Comment_ss_TE, Comment_ss_TL, Comment_ss_omega},'Comments');
-set(MSGBOX, 'position', [100 220 550 150]);
+set(MSGBOX, 'position', [50 50 450 150]);
 ah = get( MSGBOX, 'CurrentAxes');
 ch = get( ah, 'Children');
 set( ch, 'FontSize', 15); 
@@ -86,7 +155,7 @@ Comment_max_omega = ['Angular Velcodity (omega): ',num2str(max_omega), ' rad/sec
 
 % Msg box of comments
 MSGBOX = msgbox({Comment_subtitle,Comment_max_i_f, Comment_max_i_a, Comment_max_TE, Comment_max_TL, Comment_max_omega},'Comments');
-set(MSGBOX, 'position', [100 220 550 150]);
+set(MSGBOX, 'position', [500 50 450 150]);
 ah = get( MSGBOX, 'CurrentAxes');
 ch = get( ah, 'Children');
 set( ch, 'FontSize', 15); 
---
+++ ElectricMachines/DC_Motors/01_SeperatelyExcitedDCMotor/05_PositionControl/README.md
@@ -0,0 +1,13 @@
+# Seperately Excited DC Motor
+
+## Objective:
+
+## Model/Developement:
+
+## Results:
+
+## Learnings/Comments/Analysis/Remember:
+
+ 
+
+
